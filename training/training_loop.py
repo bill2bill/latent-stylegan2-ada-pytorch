5591cb146f1f3a655edabfe8ef8ac60b14efb412
bill2bill <will.fussey@gmail.com>
2022-07-14 22:45:18 +0100

    Update

--- training/training_loop.py
+++ training/training_loop.py
@@ -328,12 +328,13 @@ def training_loop(
             phase_real_img, phase_real_c = next(training_set_iterator)
 
             # The Autoencoder converts data to float and attaches to the correct device
-            if not encode:
+            if encode:
+                phase_real_img = phase_real_img.split(batch_gpu)
+            else:
                 phase_real_img = phase_real_img.to(torch.float32).to(device)
                 # Converts to a 0 - 1 range instaed of 0 - 255
                 phase_real_img = (phase_real_img / 127.5 - 1).split(batch_gpu)
             
-            phase_real_img = phase_real_img.split(batch_gpu)
             phase_real_c = phase_real_c.to(device).split(batch_gpu)
             all_gen_z = torch.randn([len(phases) * batch_size, G.z_dim], device=device)
             all_gen_z = [phase_gen_z.split(batch_gpu) for phase_gen_z in all_gen_z.split(batch_size)]
