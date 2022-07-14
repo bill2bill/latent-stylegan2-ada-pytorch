274e9d37cd3f8f2152ce5b53261ebd87f9b9d439
bill2bill <will.fussey@gmail.com>
2022-07-14 23:21:49 +0100

    update

--- training/training_loop.py
+++ training/training_loop.py
@@ -183,7 +183,7 @@ def training_loop(
 
             def __next__(self):
                 img, labels = next(self.iterator)
-                img = torch.HalfTensor(img, device=device)
+                img = torch.HalfTensor(img).to(device)
                 encoded = autoencoder.encode(img)
                 del img
                 return encoded, labels
