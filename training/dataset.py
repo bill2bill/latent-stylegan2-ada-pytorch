2093cc0c1635dcf7354ea93d254c2b6bf1b07e7e
bill2bill <will.fussey@gmail.com>
2022-07-14 23:19:11 +0100

    Update

--- training/dataset.py
+++ training/dataset.py
@@ -188,7 +188,9 @@ class ImageFolderDataset(Dataset):
 
         raw_image = self._load_raw_image(0)
         if ae:
-            raw_image = ae.encode(np.expand_dims(raw_image, 0))[0]
+            raw_image = raw_image.to(ae.device)
+            
+            raw_image = ae.encode(torch.unsqueeze(raw_image, 0))[0].cpu().detach().numpy()
 
         raw_shape = [len(self._image_fnames)] + list(raw_image.shape)
 
