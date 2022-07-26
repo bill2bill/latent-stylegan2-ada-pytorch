a042ebd442f105a1baf416fe82d76dfed5e53e15
bill2bill <will.fussey@gmail.com>
2022-07-26 13:05:55 +0100

    w

--- training/dataset.py
+++ training/dataset.py
@@ -300,7 +300,7 @@ class EncodedDataset(torch.utils.data.Dataset):
         cache_dir = f"{get_cache_dir()}/latent_images"
         self._cache_dir = cache_dir
         if cache:
-            max_idx = max(list(map(lambda path : re.findall(r'\d+', path)[0], os.listdir(cache_dir))))
+            max_idx = max(list(map(lambda path : int(filter(str.isdigit, path)), os.listdir(cache_dir))))
             block = len(os.listdir(cache_dir)) // ngpu
             start = rank * block
             self._start = start
