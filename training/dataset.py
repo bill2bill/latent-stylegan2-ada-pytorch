04e2e37ebf67ec8f8e0f9992d6179c70c8688d9f
bill2bill <will.fussey@gmail.com>
2022-07-26 13:56:21 +0100

    w

--- training/dataset.py
+++ training/dataset.py
@@ -374,7 +374,7 @@ class EncodedDataset(torch.utils.data.Dataset):
 
     def __iter__(self):
         self.idx = 0
-        return iter(lambda *arg: next(self), None)
+        return self
 
     def __next__(self):
         elem = self.__getitem__(self.idx)
