bc557bc483d90ec5f2bb9c84ea820f7bfc41399f
bill2bill <will.fussey@gmail.com>
2022-07-29 20:02:00 +0100

    w

--- metrics/metric_utils.py
+++ metrics/metric_utils.py
@@ -250,7 +250,7 @@ def compute_feature_stats_for_generator(opts, detector_url, detector_kwargs, rel
     # Setup generator and load labels.
     G = copy.deepcopy(opts.G).eval().requires_grad_(False).to(opts.device)
     if opts.encode:
-        print(opts)
+        print(opts.dataset_kwargs)
         dataset = EncodedDataset(**opts.dataset_kwargs)
     else:
         dataset = dnnlib.util.construct_class_by_name(**opts.dataset_kwargs)
